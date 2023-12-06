f77661d1cbc8986a3ccf56add9afd51aef62cfc9
aws1313 <69417093+aws1313@users.noreply.github.com>
2023-12-06 18:30:39 +0100

    sync

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,4 +1,3 @@
-
 import 'package:aesapp/objects/theme.dart';
 import 'package:aesapp/static/app.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +9,8 @@ import 'aesapp/appbar.dart';
 final logger = Logger("home");
 
 class HomePage extends StatefulWidget {
-  const HomePage({this.calledAsWidget=false, super.key});
+  const HomePage({this.calledAsWidget = false, super.key});
+
   final bool calledAsWidget;
 
   @override
@@ -35,37 +35,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: (!widget.calledAsWidget)?CustomAppBar.get(title: "Home"):null,
-      body: Column(children: [
-        SizedBox(
-          width: 100,
-          child: TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-                hintText: "Filter",
-                border: OutlineInputBorder()
+      appBar: (!widget.calledAsWidget) ? CustomAppBar.get(title: "Home") : null,
+      body: Column(
+        children: [
+          SizedBox(
+            width: 100,
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                  hintText: "Filter", border: OutlineInputBorder()),
             ),
           ),
-
-        ),
-        ElevatedButton(
-            onPressed: ()async{
-              //AESAppUtils.showErrorToast(Navigator.canPop(context).toString());
-              ScaffoldMessenger.of(context).showMaterialBanner(
-              MaterialBanner(
-                leading: Icon(Icons.wifi_off, color: Colors.black,),
-                contentTextStyle: TextStyle(color: Colors.black),
-                content: Text("Keine Verbindung zum Server"),
-                dividerColor: Colors.transparent,
-                actions: [Container()],
-                backgroundColor: Get.find<AESTheme>().yellow,
-              ));
-              // send to endpoint
-
-            },
-            child: const Text("setupFCM")
-        ),
-      ],),
+          ElevatedButton(
+              onPressed: () async {
+                //AESAppUtils.showErrorToast(Navigator.canPop(context).toString());
+                ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+                  leading: const Icon(
+                    Icons.wifi_off,
+                    color: Colors.black,
+                  ),
+                  contentTextStyle: const TextStyle(color: Colors.black),
+                  content: const Text("Keine Verbindung zum Server"),
+                  dividerColor: Colors.transparent,
+                  actions: [
+                    Builder(builder: (BuildContext context) {
+                      return IconButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).clearMaterialBanners();
+                        },
+                        icon: const Icon(Icons.close),
+                        color: Colors.black,
+                      );
+                    })
+                  ],
+                  backgroundColor: Get.find<AESTheme>().yellow,
+                ));
+                // send to endpoint
+              },
+              child: const Text("setupFCM")),
+        ],
+      ),
     );
   }
 }
