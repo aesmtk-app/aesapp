e27dd659b3cdeaa78c27cb266580f25a1c400928
aws1313 <69417093+aws1313@users.noreply.github.com>
2024-01-26 12:28:21 +0100

    data_provider.dart for news

--- lib/objects/news.dart
+++ lib/objects/news.dart
@@ -1,21 +1,28 @@
 import 'package:aesapp/generated/l10n.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'news.g.dart';
 part 'news.freezed.dart';
 @unfreezed
-class NewsPreview with _$NewsPreview{
+class NewsPreview extends HiveObject with _$NewsPreview{
+  @HiveType(typeId: 4, adapterName: "NewsPreviewAdapter")
   factory NewsPreview({
+    @HiveField(1)
     required String id,
-    @JsonKey(name: "published_at")
+    @JsonKey(name: "published_at") @HiveField(2)
     required DateTime published,
+    @HiveField(3)
     required String title,
-    @JsonKey(name: "preview_text")
+    @JsonKey(name: "preview_text") @HiveField(4)
     required String preview,
+    @HiveField(5)
     required String source,
+    @HiveField(6)
     required String author,
   })=_NewsPreview;
+  NewsPreview._();
 
   factory NewsPreview.fromJson(Map<String, dynamic> json) => _$NewsPreviewFromJson(json);
 
--- lib/ui/news/news_page.dart
+++ lib/ui/news/news_page.dart
@@ -1,4 +1,5 @@
 import 'package:aesapp/helpers/api.dart';
+import 'package:aesapp/helpers/data_provider.dart';
 import 'package:aesapp/ui/news/news_article.dart';
 import 'package:aesapp/ui/news/news_preview_card.dart';
 import 'package:flutter/material.dart';
@@ -18,18 +19,28 @@ class NewsPage extends StatefulWidget {
 
 class _NewsPageState extends State<NewsPage> {
   NewsPreview? selectedArticle;
+  Stream<List<NewsPreview>> newsPreviews = Get.find<DataProvider>().allNews();
+
+  Widget _loadMoreButton(){
+    return ElevatedButton(onPressed: ()=>Get.find<DataProvider>().getMoreNews(), child: Text("Weitere 15 laden"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: (!widget.calledAsWidget)?CustomAppBar.get(title: "News"):null,
-      body: FutureBuilder(
+      body: StreamBuilder(
         builder: (BuildContext context, AsyncSnapshot<List<NewsPreview>> snap){
           if(snap.hasData){
+            print("build");
             List<NewsPreview> pres = snap.data!;
             if(context.isPortrait){
               return ListView(
-                children: pres.map((e)
-                => GestureDetector(onTap: ()async{NewsArticle t =(await Get.find<API>().getArticle(e.id));Get.to(()  =>NewsArticlePage(t));},child: NewsPreviewCard(e),)).toList(),
+                children: [
+                  ...pres.map((e)
+                  => GestureDetector(onTap: ()async{NewsArticle t =(await Get.find<API>().getArticle(e.id));Get.to(()  =>NewsArticlePage(t));},child: NewsPreviewCard(e),)).toList(),
+                  _loadMoreButton()
+                ],
               );
             }
             return Row(
@@ -37,24 +48,27 @@ class _NewsPageState extends State<NewsPage> {
                 Flexible(
                     flex: 550,
                     child: ListView(
-                      children: pres.map((e) => ListTile(
-                            title: Column(
-                              children: [
-                                Text(e.title),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [Text(AESAppUtils.monthDayFormat.format(e.published))],
-                                )
-                              ],
-                            ),
-                            onTap: ()async{
-                              setState(() {
-                                selectedArticle=e;
-                              });
-                            },
-                            selected: e==selectedArticle,
+                      children: [
+                        ...pres.map((e) => ListTile(
+                          title: Column(
+                            children: [
+                              Text(e.title),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [Text(AESAppUtils.monthDayFormat.format(e.published))],
+                              )
+                            ],
+                          ),
+                          onTap: ()async{
+                            setState(() {
+                              selectedArticle=e;
+                            });
+                          },
+                          selected: e==selectedArticle,
 
                         )).toList(),
+                        _loadMoreButton()
+                      ],
                     )
 
                 ),
@@ -70,7 +84,7 @@ class _NewsPageState extends State<NewsPage> {
           }
           return Center(child: CircularProgressIndicator());
           },
-          future: Get.find<API>().getAllArticles("1","15"),
+          stream: newsPreviews,
       ),
     );
   }
